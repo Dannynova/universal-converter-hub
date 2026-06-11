2149c65665f23e95ce9f29b42fcd85d75991c944
Dannynova <okedeleadebayo40@gmail.com>
2026-06-11 22:10:38 +0100

    Add files via upload

---
+++ tmp_mupdf_info.py
@@ -0,0 +1,4 @@
+import fitz, inspect
+print('replace_image signature:', inspect.signature(fitz.Page.replace_image))
+print('get_images signature:', inspect.signature(fitz.Page.get_images))
+print('doc extract_image signature:', inspect.signature(fitz.Document.extract_image))
